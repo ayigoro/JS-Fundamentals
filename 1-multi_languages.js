351179f7eb09690bb56e323ecdb79da61c6a0375
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-09-11 22:45:31 +0100

    js foundamental files added

---
+++ 1-multi_languages.js
@@ -0,0 +1,8 @@
+// console.log("C is fun")
+// console.log("Python is cool")
+// console.log("JavaScript is amazing")
+
+const message = ["C is fun", "Python is cool", "JavaScript is amazing"]
+for (i = 0; i < message.length; i++){
+  console.log(message[i])
+}
---
+++ 4-concat.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2)
+if (args.length == 2){
+  console.log(`${args[0]} is ${args[1]}`)
+}
+else if (args.length == 1){
+  console.log(`${args} is undefined`)
+}
+else if (args.length == 0){
+  console.log("undefined is undefined")
+}
