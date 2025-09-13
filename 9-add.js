41d1703ac4215b01a0324b1c418d69d4482be1f3
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-09-13 23:11:52 +0100

    Added 9-add file

--- 9-add.js
+++ 9-add.js
@@ -1,29 +1,21 @@
 #!/usr/bin/node
 
-// Define the add function
+// Function to add two integers
 function add(a, b) {
   return a + b;
 }
 
-// Get the first and second arguments
-const args1 = process.argv[2];
-const args2 = process.argv[3];
+// Get arguments from the command line
+// process.argv[2] = first number
+// process.argv[3] = second number
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 
-// Convert arguments to integers
-const num1 = parseInt(args1, 10);
-const num2 = parseInt(args2, 10);
-
-// Check if both are valid numbers
-
-if (args1 === undefined && args2 === undefined) {
+// Check if arguments are numbers
+if (isNaN(num1) || isNaN(num2)) {
   console.log('Missing numbers');
-} else if (args2 === undefined) {
+}else if (num2 == undefined){
   console.log('Missing second number');
-}
-else if (isNaN(num1) || isNaN(num2)) {
-  console.log('Please provide two valid integers');
-}
-else  {
-  // Print the sum
+} else {
   console.log(add(num1, num2));
 }
