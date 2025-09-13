b81d5d172a58c29fcba19cf2bfecddbd8e3f87e5
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-09-13 14:57:51 +0100

    Added 6-multi_language_loop file

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+//Script that print 3 lines using for loop
+const message = ["C is fun", "Python is cool", "JavaScript is amazing"]
+for (i = 0; i < message.length; i++){
+  console.log(message[i])
+}
