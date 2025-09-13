e8dbe4275c9f1a4700a21c138c96a611d7c23cae
Ibrahim Abdulraheem <ibabdul707@gmail.com>
2025-09-13 22:45:17 +0100

    Added 9-add

--- 9-add.js
+++ 9-add.js
@@ -14,9 +14,16 @@ const num1 = parseInt(args1, 10);
 const num2 = parseInt(args2, 10);
 
 // Check if both are valid numbers
-if (isNaN(num1) || isNaN(num2)) {
+
+if (args1 === undefined && args2 === undefined) {
+  console.log('Missing numbers');
+} else if (args2 === undefined) {
+  console.log('Missing second number');
+}
+else if (isNaN(num1) || isNaN(num2)) {
   console.log('Please provide two valid integers');
-} else {
+}
+else  {
   // Print the sum
   console.log(add(num1, num2));
 }
